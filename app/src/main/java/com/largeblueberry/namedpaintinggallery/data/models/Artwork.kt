7060aceae2d549a984204c9bec9b3ce7092fe16a
largeblueberry <onlym3480@naver.com>
2024-11-22 10:31:15 +0900

    ArtworkAdapter 수정, artitem 모델 디자인, 메인액티비티 디자인,artwork 데이터 클래스 생성

---
+++ app/src/main/java/com/largeblueberry/namedpaintinggallery/data/models/Artwork.kt
@@ -0,0 +1,7 @@
+package com.largeblueberry.namedpaintinggallery.data.models
+
+data class Artwork(
+    val title: String = "",
+    val artistName: String = "",
+    val imageId: String = ""
+)
---
+++ app/src/main/java/com/largeblueberry/namedpaintinggallery/ui/adapter/ArtworkAdapter.kt
@@ -0,0 +1,34 @@
+package com.largeblueberry.namedpaintinggallery.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.largeblueberry.namedpaintinggallery.data.models.Artwork
+import com.largeblueberry.namedpaintinggallery.databinding.ArtitemBinding
+
+class ArtworkAdapter(
+    private  val artworks: List<Artwork>,
+    private  val onLoadImage: (String,ImageView)-> Unit
+
+): RecyclerView.Adapter<ArtworkAdapter.ArtworkViewHolder>() {
+    inner class ArtworkViewHolder(private val binding: ArtitemBinding):RecyclerView.ViewHolder(binding.root){
+        fun bind(artwork: Artwork) {
+            binding.artTitle.text = artwork.title
+            binding.artistName.text = artwork.artistName
+            onLoadImage(artwork.imageId, binding.artImage)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArtworkViewHolder {
+        val binding = ArtitemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ArtworkViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ArtworkViewHolder, position: Int) {
+        holder.bind(artworks[position])
+    }
+
+    override fun getItemCount(): Int = artworks.size
+
+}
